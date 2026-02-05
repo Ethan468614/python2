2bb5ef5cf1dc67e6ce36abbf22d05e9b4f0ce11b
Ethan468614 <ethancherian555@gmail.com>
2026-02-05 12:47:55 +0530

    dog breed

--- project.py
+++ project.py
@@ -1,7 +1,22 @@
 class Dog:
+    species = "Canis familiaris"  
 
-    def __init__(self, breed, color):
-        self.breed = breed
-        self.color = color
+    def __init__(self, breed, name):
+        self.breed = breed          
+        self.name = name            
+
+    def display_info(self):
+        print(f"Dog Name: {self.name}")
+        print(f"Breed: {self.breed}")
+        print(f"Species: {Dog.species}")
+
+dog1 = Dog("Labrador", "Buddy")
+dog2 = Dog("German Shepherd", "Max")
+dog1.display_info()
+print()  
+dog2.display_info()
+
+
+
 
         
