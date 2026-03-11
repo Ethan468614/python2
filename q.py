af750ed4be85b7268b2da6c7a1ccfcff3ebe02f2
Ethan468614 <ethancherian555@gmail.com>
2026-03-11 10:47:38 +0530

    top level window

---
+++ q.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("400x300")
+root.title("main")
+
+def topwin():
+
+    top = Toplevel()
+
+    top.geometry("180x100")
+
+    l2 = Label(top, text="this is a toplevel window")
+    l2.pack()
+
+    top.mainloop()
+
+
+l = Label(root, text="This is a root window")\
+
+btn = Button(root, text="click here to open another window", command=topwin)    
+
+l.pack()
+btn.pack()
+
+root.mainloop()
