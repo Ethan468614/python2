19bde0976931c266f3cd7e4b652d5d3674f4e751
Ethan468614 <ethancherian555@gmail.com>
2026-03-26 10:35:51 +0530

    bot

---
+++ revise.py
@@ -0,0 +1,21 @@
+print("hello i am an ai bot. Whats your name?")
+
+name = input()
+
+print(f"Nice to meet you, {name}! ")
+
+print("How are you feeling today? good/bad")
+
+mood = input().lower()
+
+if mood == "good":
+    print("Im glad to hear that")
+
+elif mood == "bad":
+    print("Im sorry to hear that. I hope your day gets better!")
+
+else:
+    print("I see, sometimes its hard to put our feelings into words. ")
+
+
+print("Goodbye! It was nice talking to you ", name)    
