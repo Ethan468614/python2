02d434fbc464388f69ef95cc916d3bea11a4d9e3
Ethan468614 <ethancherian555@gmail.com>
2026-04-10 11:38:23 +0530

    brited image

---
+++ edit2.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread('example.jpg')
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+(h, w) = image.shape[:2]
+center = (w//2, h//2)
+M = cv2.getRotationMatrix2D(center, 45, 1.0)
+
+rotated =  cv2.warpAffine(image_rgb, M, (w, h))
+
+plt.imshow(rotated)
+plt.title('Rotated Image')
+plt.show()
+
+brightness_matrix = np.ones(image_rgb.shape, dtype='uint8') * 50
+briter =  cv2.add(image_rgb, brightness_matrix)
+
+briter_rgb = cv2.cvtColor(briter, cv2.COLOR_BGR2RGB)
+plt.imshow(briter_rgb)
+plt.title('Brightened Image')
+plt.show()
